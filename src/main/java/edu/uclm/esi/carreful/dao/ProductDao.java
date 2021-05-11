3db1353838acc1f72a81b7df03e9773a5ebf7bd7
chemabr24 <JoseManuel.Bastante@alu.uclm.es>
2021-05-11 16:16:47 +0200

    categorias

--- src/main/java/edu/uclm/esi/carreful/dao/ProductDao.java
+++ src/main/java/edu/uclm/esi/carreful/dao/ProductDao.java
@@ -1,6 +1,10 @@
 package edu.uclm.esi.carreful.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import edu.uclm.esi.carreful.model.Product;
@@ -11,4 +15,7 @@ import edu.uclm.esi.carreful.model.Product;
 public interface ProductDao extends JpaRepository <Product, String> {
 
 	Product findByNombre(String nombre);
+
+	@Query(value ="SELECT * FROM carreful.product p, carreful.categoria c WHERE p.categoria_id=c.id and c.nombre=:categoria", nativeQuery = true)
+	List<Product> findProduct(@Param ("categoria")String categoria);
 }
--- src/main/webapp/js/viewModels/product.js
+++ src/main/webapp/js/viewModels/product.js
@@ -1,11 +1,24 @@
 define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
-		'jquery', 'twbsPagination' ], function(ko, app, moduleUtils, accUtils, $) {
+		'jquery' ], function(ko, app, moduleUtils, accUtils, $) {
 
 	class ProductViewModel {
 		constructor() {
+			
 			var self = this;
 			self.productos = ko.observableArray([]);
 			self.carrito = ko.observableArray([]);
+			self.selectedCategory = ko.observable();
+
+			self.categorias = ko.observableArray(["Todos"]);
+			self.selectionChanged = function() {
+					this.getProductCategoria();
+				
+
+				
+		   } 
+
+		  
+       		
 
 			self.message = ko.observable(null);
 			self.error = ko.observable(null);
@@ -23,9 +36,11 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 					'viewModel' : app.getHeaderModel()
 				})
 			})
+
+			
 		}
 
-		paginacion() {
+		/*paginacion() {
 			$('#pagination').twbsPagination({
 				currentPage: page.currentPage,
 				totalPages: Math.ceil(page.totalCount/page.pageSize),
@@ -42,7 +57,7 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 				}
 			});
 			 
-			}
+			}*/
 
 		add() {
 			var self = this;
@@ -70,6 +85,41 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 			let self = this;
 			let data = {
 				url: "product/getTodos",
+				type: "get",
+				contentType: 'application/json',
+				success: function (response) {
+					self.productos(response);					
+				},
+				error: function (response) {
+					self.error(response.responseJSON.errorMessage);
+				}
+			};
+			$.ajax(data);
+		}
+
+		getCategorias(){
+			let self = this;
+			let data = {
+				url: "product/getCategorias",
+				type: "get",
+				contentType: 'application/json',
+				success: function (response) {
+					self.categorias(response);					
+				},
+				error: function (response) {
+					self.error(response.responseJSON.errorMessage);
+				}
+			};
+			$.ajax(data);
+		}
+
+		
+
+		getProductCategoria(){
+			let self = this;
+			let categoria = self.selectedCategory();
+			let data = {
+				url: "product/getProductos/" + categoria,
 				type: "get",
 				contentType: 'application/json',
 				success: function (response) {
@@ -123,8 +173,10 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 		connected() {
 			accUtils.announce('Login page loaded.');
 			document.title = "Login";
-			this.getProductos();
-			this.paginacion();
+			this.getCategorias();
+			//this.getProductos();
+			
+		//	this.paginacion();
 
 		};
 
@@ -135,7 +187,11 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 		transitionCompleted() {
 			// Implement if needed
 		};
+		todos(){
+			console.log("Ha señalado TODOS");
+		}
 	}
+		
 
 	return ProductViewModel;
 });
